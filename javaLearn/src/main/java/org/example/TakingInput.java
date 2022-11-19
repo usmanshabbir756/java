ca3cda5739cc9ffad94ce7375d7bcff18f0c555b
usman <shabbir.usman3314@gmail.com>
2022-11-19 11:55:58 +0500

    input from user

---
+++ javaLearn/src/main/java/org/example/TakingInput.java
@@ -0,0 +1,15 @@
+package org.example;
+import java.util.Scanner;
+public class TakingInput {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter First name ");
+        String fName=input.next();
+        System.out.println("Enter Last name ");
+        String lName=input.next();
+        System.out.println("Hello "+ fName + lName);
+
+
+
+    }
+}
