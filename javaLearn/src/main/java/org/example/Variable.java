16734fc22e1b75c6907707728550cc549746278a
usman <shabbir.usman3314@gmail.com>
2022-11-19 11:24:29 +0500

    variables premitive types

---
+++ javaLearn/src/main/java/org/example/Variable.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Variable {
+    public static void main(String[] args) {
+        System.out.print("the sum is : ");
+        byte num1=9;
+        byte num2=9;
+        int sum = num1 + num2;
+        System.out.println(sum);
+        float num3=0.5f;
+        double num4=0.456;
+        char alphabet='A';
+        System.out.println(alphabet);
+    }
+}
