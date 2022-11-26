92ffc670d9e5f53c33ac2ebde2a9c9e1044f29c1
usman <usmanshabbir756@gmail.com>
2022-11-26 12:35:02 +0500

    loop practice in javas

---
+++ javaLearn/src/main/java/org/example/loop.java
@@ -0,0 +1,53 @@
+package org.example;
+import java.util.Scanner;
+
+public class loop {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+//        //table
+//        for (int i=1;i<=10;i++) {
+//            System.out.println("2 * " + i + " : " + 2 * i);
+//        }
+//        //Star
+//        for (int j=4;j>=1;j--){
+//            for (int k=1;k<=j;k++){
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+
+//        //sum of even number
+//        System.out.print("Enter n number to find sum if even number : ");
+//        int a=sc.nextInt();
+//        int b=0;
+//        int sum=0;
+//        while(b<=a){
+//            if(b%2==0){
+//                sum+=b;
+//            }
+//            b++;
+//        }
+//        System.out.println(sum);
+
+//        //Factorial
+//        System.out.print("Enter number for factorial : ");
+//        int f=sc.nextInt();
+//        int m=1;
+//        for (int h=f;h>=1;h--){
+//            m*=h;
+//        }
+//        System.out.println(m);
+
+
+//        //sum of table
+//        System.out.print("enter number of table for  sum :");
+//        int t=sc.nextInt();
+//        int sum=0;
+//        for (int i=1;i<=10;i++){
+//            sum=sum+(t*i);
+//        }
+//        System.out.println(sum);
+
+        
+    }
+}
